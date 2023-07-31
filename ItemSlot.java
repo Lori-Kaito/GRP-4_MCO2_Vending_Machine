3424a952ec8b203854eab5061f5a3c96f0b61040
RK <64896956+Lori-Kaito@users.noreply.github.com>
2023-07-31 21:12:07 +0800

    Add files via upload

---
+++ ItemSlot.java
@@ -0,0 +1,21 @@
+public class ItemSlot {
+    private Item item;
+    private int quantity;
+
+    public ItemSlot(Item item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
